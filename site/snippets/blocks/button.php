d2e90a436f73c073267e0d7a8325cb9859a0d405
Manuel Deutsch <hi@manuel-deutsch.de>
2021-12-22 15:12:18 +0100

    add button example

---
+++ site/snippets/blocks/button.php
@@ -0,0 +1,12 @@
+<a href="<?= $block->link() ?>" class="button">
+  <span>
+    <svg xmlns="http://www.w3.org/2000/svg" width="11.263" height="10.91" viewBox="0 0 11.263 10.91">
+      <g id="Gruppe_499" data-name="Gruppe 499" transform="translate(-1357.473 -2094.545)">
+        <path id="Pfad_171" data-name="Pfad 171" d="M-239.932,1891.969l5.1,5.1-5.1,5.1"
+          transform="translate(1602.86 202.93)" fill="none" stroke="#2b2b38" stroke-width="1" />
+        <path id="Pfad_172" data-name="Pfad 172" d="M-239.932,1891.969l5.1,5.1-5.1,5.1"
+          transform="translate(1597.759 202.93)" fill="none" stroke="#2b2b38" stroke-width="1" />
+      </g>
+    </svg>
+    <?= $block->text() ?><span>
+</a>

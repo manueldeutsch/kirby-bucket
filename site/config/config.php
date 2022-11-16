2c81ff396122ebb3696c8f612134529cb2021ff0
Manuel Deutsch <hi@manuel-deutsch.de>
2022-11-16 13:45:26 +0100

    fixes

--- site/config/config.php
+++ site/config/config.php
@@ -5,28 +5,30 @@ return [
     "panel" => [
         "install" => true,
     ],
-    'routes' => [
+    "routes" => [
         [
-          'pattern' => 'sitemap.xml',
-          'action'  => function() {
-              $pages = site()->pages()->index();
-    
-              // fetch the pages to ignore from the config settings,
-              // if nothing is set, we ignore the error page
-              $ignore = kirby()->option('sitemap.ignore', ['error']);
-    
-              $content = snippet('sitemap', compact('pages', 'ignore'), true);
-    
-              // return response with correct header type
-              return new Kirby\Cms\Response($content, 'application/xml');
-          }
+            "pattern" => "sitemap.xml",
+            "action" => function () {
+                $pages = site()
+                    ->pages()
+                    ->index();
+
+                // fetch the pages to ignore from the config settings,
+                // if nothing is set, we ignore the error page
+                $ignore = kirby()->option("sitemap.ignore", ["error"]);
+
+                $content = snippet("sitemap", compact("pages", "ignore"), true);
+
+                // return response with correct header type
+                return new Kirby\Cms\Response($content, "application/xml");
+            },
         ],
         [
-          'pattern' => 'sitemap',
-          'action'  => function() {
-            return go('sitemap.xml', 301);
-          }
-        ]
+            "pattern" => "sitemap",
+            "action" => function () {
+                return go("sitemap.xml", 301);
+            },
+        ],
     ],
-    'sitemap.ignore' => ['error'],
+    "sitemap.ignore" => ["error"],
 ];

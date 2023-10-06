4ae19fb45c0b90a63101298b92bf62d24e5a6e6b
Manuel Deutsch <hi@manuel-deutsch.de>
2023-10-06 23:45:09 +0200

    button fix

--- site/snippets/blocks/button.php
+++ site/snippets/blocks/button.php
@@ -1,3 +1,3 @@
-<a href="<?= $block->link() ?>" class="button <?= $block->class() ?>" <?= $block->external()->toBool() ? "target='_blank'" : "" ?>>
+<a href="<?= $block->external()->toBool() ? $block->link() : $site->url() . $block->link() ?>" class="button <?= $block->class() ?>" <?= $block->external()->toBool() ? "target='_blank'" : "" ?>>
   <span><?= $block->text() ?></span>
 </a>
